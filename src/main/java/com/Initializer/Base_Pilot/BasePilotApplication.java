2115b5d744b9a856fd53ea830fd29c29e7d812d5
Montassar Lassoued <montassar.lassoued.4231@gmail.com>
2025-11-01 22:36:52 +0100

    neues Basis-Projekt hinzugefügt

---
+++ src/main/java/com/Initializer/Base_Pilot/BasePilotApplication.java
@@ -0,0 +1,13 @@
+package com.Initializer.Base_Pilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasePilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasePilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Initializer/Base_Pilot/BasePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Initializer.Base_Pilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasePilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
