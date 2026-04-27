fce97e3fb79c9882ad7fed8c5948c18a62035167
Montassar Lassoued <montassar.lassoued.4231@gmail.com>
2026-04-27 10:35:09 +0200

    Spring Security User-Objekt anstatt eigenes User-Objekt

--- IntraConnect-UI/src/main/java/com/IntraConnect/services/CustomUserDetailsService.java
+++ IntraConnect-UI/src/main/java/com/IntraConnect/services/CustomUserDetailsService.java
@@ -17,45 +17,37 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private static final Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
     private com.IntraConnect.user.User user;
-
-    @Override
-    public UserDetails loadUserByUsername(String _email) throws UsernameNotFoundException {
-
-        log.info("inside loadUserByUsername{}",_email);
-
-        String query = "SELECT " +
-                "APPUSERS.USERNAME, " +
-                "APPUSERS.EMAIL, " +
-                "APPUSERS.PASSWORD, " +
-                "ROLE.ROLE " +
-                "FROM APPUSERS " +
-                "LEFT JOIN ROLE  ON ROLE.id = APPUSERS.role_id " +
-                "WHERE APPUSERS.EMAIL = '" + _email + "'";
-
-        try(Transaction transaction = Transaction.create()){
-            ResultSet rs = transaction.select(query);
-            if (!rs.next()) {
-                throw new UsernameNotFoundException("User not found: " + _email);
-            }
-            String name = rs.getString("USERNAME");
-            String email = rs.getString("EMAIL");
-            String password = rs.getString("PASSWORD");
-            String role = rs.getString("ROLE"); // "ADMIN" oder "USER"
-
-            user = new com.IntraConnect.user.User(name,email, password, role);
-
-            return User.withUsername(email)
-                    .password(password)
-                    .roles(role)  // Spring macht ROLE_ automatisch
-                    .build();
-
-        }catch (Exception e){
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    public com.IntraConnect.user.User getUser() {
-        return user;
-    }
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		log.info("Lade User Details für: {}", email);
+		
+		String query = "SELECT " +
+				"APPUSERS.USERNAME, " +
+				"APPUSERS.EMAIL, " +
+				"APPUSERS.PASSWORD, " +
+				"ROLE.ROLE " +
+				"FROM APPUSERS " +
+				"LEFT JOIN ROLE ON ROLE.id = APPUSERS.role_id " +
+				"WHERE APPUSERS.EMAIL = ?";
+		
+		try (Transaction transaction = Transaction.create()) {
+			ResultSet rs = transaction.select(query, email);
+			
+			if (!rs.next()) {
+				throw new UsernameNotFoundException("User nicht gefunden: " + email);
+			}
+			
+			// Wir geben ein sauberes Spring Security User-Objekt zurück
+			return User.withUsername(rs.getString("EMAIL"))
+					.password(rs.getString("PASSWORD"))
+					.roles(rs.getString("ROLE"))
+					.build();
+			
+		} catch (Exception e) {
+			log.error("Datenbankfehler in CustomUserDetailsService: ", e);
+			throw new UsernameNotFoundException("Fehler beim Laden des Users", e);
+		}
+	}
+	
 }
