cbf6278b22028bef0292ea4dfb05c5d05b671204
Montassar Lassoued <montassar.lassoued.4231@gmail.com>
2026-04-27 10:13:36 +0200

    **jwt.secret: Windows -> $env:JWT_SECRET="DeinGanzLangesSicheresSecretHier" java -jar dein-backend-modul.jar
    Linux-> export JWT_SECRET=DeinGanzLangesSicheresSecretHier
            java -jar dein-backend-modul.jar
    Docker -> services:
                backend:
                  image: dein-backend:latest
                  environment:
                    - JWT_SECRET=DeinGanzLangesSicheresSecretHier

--- IntraConnect-UI/src/main/java/com/IntraConnect/jwt/JwtUtil.java
+++ IntraConnect-UI/src/main/java/com/IntraConnect/jwt/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,14 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private final String secret= "MCtJdRxC7EYXYMMTdlGSg7ZhpszneEkXZSfSjBnpG9Z";
-    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
+    /*private final String secret= "MCtJdRxC7EYXYMMTdlGSg7ZhpszneEkXZSfSjBnpG9Z";
+    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());*/
+	private final Key key;
+	// Spring sucht nach einer Umgebungsvariable namens JWT_SECRET
+	// oder einem Eintrag jwt.secret in den Properties.
+	public JwtUtil(@Value("${jwt.secret}") String secret) {
+		this.key = Keys.hmacShaKeyFor(secret.getBytes());
+	}
 
     public  String extractUsername(String token){
         return extractClaims(token, Claims::getSubject);
